9570f77d5f3fdc0982ce9f8e887f9d5755031853
Tanmay-1122 <tanmaysonawane989@email.com>
2026-03-06 19:20:03 +0530

    removed the mock data

--- src/lib/medusa.js
+++ src/lib/medusa.js
@@ -1,8 +1,8 @@
 import Medusa from "@medusajs/js-sdk"
 
 export const medusa = new Medusa({
-    baseUrl: "https://sb-j801w537xesf.ai.prod.medusajs.cloud",
-    publishableKey: "pk_ba6579fa035cf262562cb6b4e99852c5ebc598456e387880b8e3aab786fb1a29",
+    baseUrl: "http://localhost:9000",
+    publishableKey: "pk_a5b8c9e33da9b5b7ab69cc8194e681dbaee7e0354cf695d99fd829865c98a4ec",
 })
 
-export const INDIA_REGION_ID = "reg_01KJWSJ75KEZWWSGV2QS46043Q"
+export const INDIA_REGION_ID = "reg_01KK1B4EDN2WDFD5AR87JKX8SQ"
