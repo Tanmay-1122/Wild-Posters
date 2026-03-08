13f06bd57d713e606099d841c620c195a80f6744
Tanmay-1122 <tanmaysonawane989@email.com>
2026-03-08 15:50:30 +0530

    your changes description

--- src/lib/medusa.js
+++ src/lib/medusa.js
@@ -1,8 +1,8 @@
 import Medusa from "@medusajs/js-sdk"
 
 export const medusa = new Medusa({
-    baseUrl: "http://localhost:9000",
-    publishableKey: "pk_a5b8c9e33da9b5b7ab69cc8194e681dbaee7e0354cf695d99fd829865c98a4ec",
+    baseUrl: "https://wild-posters-backendd-r8lq.onrender.com",
+    publishableKey: "pk_f5245a6cc9c8c35c01df1a9e40d8f97b22d1b33a0b873789a4ec63fded2c3291",
 })
 
-export const INDIA_REGION_ID = "reg_01KK1B4EDN2WDFD5AR87JKX8SQ"
+export const INDIA_REGION_ID = "reg_01KK6CVS444QZHVHR7SC81Y2DJ"
